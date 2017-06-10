f13974d7604701cd8168e1dce2462f70da64edc6
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-06-10 15:38:38 +0800

    using postgres

--- buildservice/settings.py
+++ buildservice/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'buildservice.wsgi.application'
 
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': POSTGRES_DATABASE,
+		'USER': POSTGRES_USER,
+		'PASSWORD': POSTGRES_SECRET,
+		'HOST': '127.0.0.1',
 	}
 }
 
