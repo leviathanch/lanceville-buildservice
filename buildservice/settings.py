23c5a1d9750be4c78eb951dc0eab8cc49b030151
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-06-10 16:48:28 +0800

    setting mail host info

--- buildservice/settings.py
+++ buildservice/settings.py
@@ -128,3 +128,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# Host for sending e-mail.
+EMAIL_HOST = 'localhost'
+
+# Port for sending e-mail.
+EMAIL_PORT = 25
+
+# Optional SMTP authentication information for EMAIL_HOST.
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
