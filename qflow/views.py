0891dc025247c379adf85ad57a116c39c3403183
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-06-14 23:42:16 +0800

    Adding default views

--- qflow/views.py
+++ qflow/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
-class Settings(TemplateView):
+class DefaultView(TemplateView):
 	template_name = 'base.html'
