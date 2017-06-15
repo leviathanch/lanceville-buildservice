2be6adec55e63542d6e174adea02afad88ae38e8
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-06-15 16:04:37 +0800

    changing default page into detailview

--- qrouter/views.py
+++ qrouter/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
+from buildservice.models import ChipDesign
 
-class DefaultView(TemplateView):
+class DefaultView(DetailView):
 	template_name = 'base.html'
+	model = ChipDesign
